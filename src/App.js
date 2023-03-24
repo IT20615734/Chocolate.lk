2adf7c6465661dfffb6e3c6c9ff2e8f9ec60d188
IT20615734 <it20615734@my.sliit.lk>
2023-03-24 14:51:51 +0530

    Environment done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Main from "./Components/Main";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chocolate Online Store</h1>
+
+      {/* this line comes from components/main.js file */}
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Main.js
@@ -0,0 +1,11 @@
+function Main() {
+  return (
+    <div>
+      <header>
+        <body>Sri Lanka's Largest Online chocolate Shopping Store.</body>
+      </header>
+    </div>
+  );
+}
+
+export default Main;
