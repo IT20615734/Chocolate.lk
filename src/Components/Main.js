76a6bc520e8065eb5c3457079ff7f4132926056b
IT20615734 <it20615734@my.sliit.lk>
2023-04-20 14:09:59 +0530

    add cover

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -18,6 +18,12 @@ function Main(props) {
         {/* define object name this line */}
 
         <p>{Objct.name}</p>
+        <div className="Cover_img">
+          <img
+            src="https://cdn.shopify.com/s/files/1/1825/7689/collections/vegan-friendly-chocolate-766869_1400x600_crop_center.jpg?v=1672827620"
+            alt="Coverimage"
+          ></img>
+        </div>
       </header>
     </div>
   );
